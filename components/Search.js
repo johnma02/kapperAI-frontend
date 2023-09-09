dc03d770a3e4034fb3d4e5caf49ec0d9bdc68ca3
Prasheel Vellanki <prasheelvellanki@gmail.com>
2023-09-09 19:28:37 -0400

    pulled main

--- components/Search.js
+++ components/Search.js
@@ -6,14 +6,26 @@ export default function Search({inSearch, setInSearch, setNewHairstyle}) {
   const [items, setItems] = useState([]);
   const [userInput, setUserInput] = useState('');
   
-  const handleSubmit = async () => {
-    fetch()
+  const handleSubmit = () => {
+    return fetch(`http://10.251.142.149:8000/search?query=${userInput}`)
+      .then(response => response.json())
+      .then(json => {
+        setItems(items => [...items, `${json.url}`])
+        return 'success';
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    };
 
-  }
+  
+
+  
     return(
         <View style={styles.body}>
             <Button style={{marginTop: 20}}onPress={()=>setInSearch(false)}>Back</Button>
             <Text>Search</Text>
+            {items != null}
             <Input
             placeholder='Search Hairstyles...'
             onChangeText={(text)=>setUserInput(text)}
