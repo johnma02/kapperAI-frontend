39e16b87f8b3710d2b21ddd739bd74e6b9631062
Prasheel Vellanki <prasheelvellanki@gmail.com>
2023-09-10 07:20:21 -0400

    broken gallery

--- components/Search.js
+++ components/Search.js
@@ -1,46 +1,68 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { useState } from 'react';
-import { Button, Input } from '@rneui/themed';
+import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, Input } from "@rneui/themed";
+import AnimatedGallery from "@akumzy/react-native-animated-gallery";
 
-export default function Search({inSearch, setInSearch, setNewHairstyle}) {
+export default function Search({ inSearch, setInSearch, setNewHairstyle }) {
   const [items, setItems] = useState([]);
-  const [userInput, setUserInput] = useState('');
-  
+  const [userInput, setUserInput] = useState("");
+
+  // does not work yet
   const handleSubmit = () => {
-    return fetch(`http://10.251.142.149:8000/search?query=${userInput}`)
-      .then(response => response.json())
-      .then(json => {
-        setItems(items => [...items, `${json.url}`])
-        return 'success';
+    return fetch(`http://10.250.132.52:8000/search?query=${userInput}`)
+      .then((response) => response.json())
+      .then((json) => {
+        setItems(json);
+        console.log("success");
+        return;
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
-    };
+  };
 
-  
+  const Loader = () => {
+    return (
+      <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+        <Text>Loading..</Text>
+      </View>
+    );
+  };
 
-  
-    return(
-        <View style={styles.body}>
-            <Button style={{marginTop: 20}}onPress={()=>setInSearch(false)}>Back</Button>
-            <Text>Search</Text>
-            {items != null}
-            <Input
-            placeholder='Search Hairstyles...'
-            onChangeText={(text)=>setUserInput(text)}
-            onSubmitEditing={handleSubmit}
-            />
-        </View>
-    )
+  return (
+    <View style={styles.body}>
+      <AnimatedGallery
+        imageUrls={items}
+        renderLoader={<Loader />}
+        disablefullScreen={false}
+        thumbBorderWidth={2}
+        thumbBorderColor={"white"}
+        spacing={8}
+        imageSize={40}
+        backgroundColor={"#0000"}
+        invertThumbDirection={false}
+        invertGalleryDirection={false}
+      />
+      <Text>Search</Text>
+      {items != null}
+      <Input
+        placeholder="Search Hairstyles..."
+        onChangeText={(text) => setUserInput(text)}
+        onSubmitEditing={handleSubmit}
+      />
+      <Button style={{ margin: "10px" }} onPress={() => setInSearch(false)}>
+        Back
+      </Button>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   body: {
-    display: 'flex',
-    flexDirection: 'column',
+    display: "flex",
+    flexDirection: "column",
     gap: 10,
-    height:'90%',
+    height: "90%",
     padding: 35,
   },
 });
